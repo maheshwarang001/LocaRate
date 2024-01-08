3ef49133f0e568de6727979cf66acb35cf6443dd
Mahesh Warang <76471375+maheshwarang001@users.noreply.github.com>
2024-01-08 21:54:40 +0000

    Update endpoints.dart

--- lib/res/api_url/endpoints.dart
+++ lib/res/api_url/endpoints.dart
@@ -2,6 +2,6 @@ class Endpoints{
 
   static const baseUrl = "maps.googleapis.com"; //base url
   static const pathNearby = "/maps/api/place/nearbysearch/json"; //path url
-  static const key = "AIzaSyDBnM19uEQErxWeOSHdikRQub5zslRtVgk";
+  static const key = "__________"; //api key contact
 
 }
